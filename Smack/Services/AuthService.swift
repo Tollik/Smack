b5f71835da55a75e22b9d5fb040c821783f100b0
Tollik <toliks1121@gmail.com>
2018-07-28 21:28:35 +0300

    added auth service

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+import Alamofire
+
+class AuthService {
+    // singleton for ensure there's only one instance of that class
+    static let instance = AuthService()
+    // basically the most simple way to saving data in your app ...
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+           defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        // just to avoid any spell checks and errors, make sure the email is lower cased
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -9,7 +9,27 @@
 
 import Foundation
 
+// completion handler to handle the http response
+// typealias is simple a renaming a type
+/*
+ For example :
+ typealias Jonny = String
+ let name: Jonny = "Jpnny"
+ basically, we just remapped a type String to a new type Jonny, which is remain the String but with different name...
+ */
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://awesomechatapp.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+

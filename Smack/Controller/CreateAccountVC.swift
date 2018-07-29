9000b6330b7e1ca819ebcb1fc18c237c6d6182fc
Tollik <toliks1121@gmail.com>
2018-07-29 12:26:02 +0300

    register user created

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -11,13 +11,37 @@ import UIKit
 class CreateAccountVC: UIViewController {
     
     // Outlets
-
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     // Actions
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        // unwrapping and creating variables to store email and pass, guaranting there's a valid value in them
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registred user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColoroPressed(_ sender: Any) {
+    }
+    
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }

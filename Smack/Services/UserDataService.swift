c2546cb37fd515413a1842dd40e929eb9f5e179a
Tollik <toliks1121@gmail.com>
2018-08-01 14:22:35 +0300

    solved avatarColor bug

--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -20,7 +20,7 @@ class UserDataService {
     
     func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
         self.id = id
-        self.avatarColor = avatarName
+        self.avatarColor = color
         self.avatarName = avatarName
         self.email = email
         self.name = name
@@ -29,5 +29,52 @@ class UserDataService {
     func setAvatarName(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+    func returnUIColor(components: String) -> UIColor{
+//"[0.184313725490196, 0.870588235294118, 0.627450980392157, 1]"
+        let scanner = Scanner(string: components) //scanner creation
+        let skipped = CharacterSet(charactersIn: "[], ") // chars to be skipped
+        let comma = CharacterSet(charactersIn: ",") // char to tell the scanner where to stop scan
+        scanner.charactersToBeSkipped = skipped // actually tell the scanner what should be skipped
+        // grabbing an optional string
+        var r, g, b, a : NSString?
+        // telling scanner to scan through objects , stop scan at comma and save the value to the variables
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        // default color incase we fail the unwrpping
+        let defaultColor = UIColor.lightGray
+        //unwrapping optional strings
+        guard let rUnwrapped = r else {return defaultColor }
+        guard let gUnwrapped = g else {return defaultColor }
+        guard let bUnwrapped = b else {return defaultColor }
+        guard let aUnwrapped = a else {return defaultColor }
+        // converting strings to doubles, because there's no other way to convert string to cgfloat
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        // generating a new color with cgfloats that we got from the strings
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        // return a new color
+        return newUIColor
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
